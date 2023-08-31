67ec53a4be68c7ddec4fee7701152fed316deb8a
Dopefish <artyom.movchan@gmail.com>
2023-08-31 11:53:03 +0300

    fix

--- controllers/users.js
+++ controllers/users.js
@@ -11,9 +11,9 @@ const getUserById = (req, res) => {
   return Users.findById(id)
     .then((user) => {
       if (!user) {
-        res.status(404).send({ message: 'User not found' });
+        return res.status(404).send({ message: 'User not found' });
       }
-      res.status(200).send(user);
+      return res.status(200).send(user);
     })
     .catch(() => res.status(500).send({ message: 'Server Error' }));
 };
